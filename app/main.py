ef5579a98660b242ed4f1a0a0190ad779cfe8f09
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-08-19 12:56:32 +0930

    fix: removed unneeded static file mounting

--- app/main.py
+++ app/main.py
@@ -5,14 +5,10 @@ Sets up static files, templates, and the root endpoint.
 
 from fastapi import FastAPI
 from fastapi.requests import Request
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.templating import _TemplateResponse
 
 app = FastAPI(title="Track Shop")
-
-# Mount static files (CSS, JS, etc.)
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Set up Jinja2 templates
 templates = Jinja2Templates(directory="app/templates")
