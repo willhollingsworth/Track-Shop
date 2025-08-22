9fe0f06a0aeff5eade08083d7069f5d2bb9fe1a3
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-08-22 13:05:39 +0930

    feat: added genres listings with dynamic genre template

--- app/main.py
+++ app/main.py
@@ -59,3 +59,22 @@ def recommended(
         "recommended.html",
         {"request": request, "tracks": tracks},
     )
+
+
+@app.get("/genre/{genre_name}")
+def genre(
+    request: Request,
+    session: Annotated[Session, Depends(get_session)],
+    genre_name: str,
+) -> object:
+    """Songs by genre."""
+    tracks = session.exec(
+        select(Track)
+        .where(func.lower(Track.genre) == genre_name.lower())
+        .order_by(func.random())
+        .limit(6),
+    ).all()
+    return templates.TemplateResponse(
+        "genre.html",
+        {"request": request, "tracks": tracks, "genre_name": genre_name},
+    )
