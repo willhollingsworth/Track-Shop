153e33f66fb7b2114a160d60efec10959bc408d9
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2025-11-11 16:16:43 +1030

    fix: updating db reset to work with new db setup

--- app/db/reset_db.py
+++ app/db/reset_db.py
@@ -1,15 +1,17 @@
 """Reset database."""
 
-from sqlmodel import SQLModel
+from sqlalchemy import text
 
-from app import models  # noqa: F401 # Required for reset to work correctly
+from app import models  # Required for reset to work correctly
 from app.db.db import engine
 
 
 def reset_db() -> None:
     """Reset the database by dropping and recreating all tables."""
-    SQLModel.metadata.drop_all(engine)
-    SQLModel.metadata.create_all(engine)
+    with engine.connect() as conn:
+        conn.execute(text("DROP SCHEMA public CASCADE; CREATE SCHEMA public;"))
+        conn.commit()
+    models.SQLModel.metadata.create_all(engine)
     print("Database reset complete.")
 
 
